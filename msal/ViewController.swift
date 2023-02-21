adb6abc4d61852283b59f4d8a4907b241f673fe1
taijusanagi <taijusanagi@gmail.com>
2023-02-21 13:59:23 +0700

    initial

--- msal/ViewController.swift
+++ msal/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func authenticateButton(_ sender: Any) {
+        print("start authenticate")
+        
+    }
+    
 }
 
